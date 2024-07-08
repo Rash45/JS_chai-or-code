548a1d497de4630f597a0e0b28d40ee0544ddd38
Rash45 <rashsinha26@gmail.com>
2024-07-08 12:10:15 +0000

    container config done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,16 @@
+const accountID= 1446649
+let accountEmail = "gita"
+var accountPassword = "484849"
+accountCity = "BBSr"
+
+
+//accountID =2
+
+console.log(accountID)
+
+/* 
+
+prefer not to use 'var' 
+beacause
+*/
+console.table([accountID,accountEmail,accountPassword,accountCity]);
