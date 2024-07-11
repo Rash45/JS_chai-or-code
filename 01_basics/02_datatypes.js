e37a98aaa2070b4a3d47dd7d049c91d2facaa808
Rash45 <rashsinha26@gmail.com>
2024-07-11 08:12:16 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version 
+
+// alert (1+1) // we are using nodeJS not browser
+
+ console.log("sinha")
+
+ let name ="hlo"
+ let age = 18
+ let isLoggedIn = false
+
+
+ // number => 2 to power 53
+ /*  
+ Bigint 
+ string => ""
+ boolean => true/false
+ null => standalone value 
+ undefined => 
+ symbol => unique
+*/
+
+console.log(typeof undefined ); //undefined
+console.log(typeof null)   // object
